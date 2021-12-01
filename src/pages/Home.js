efe293a315e206a992c961028b2d8ce154d4610c
Bobby Sanchez <sanchezbobby93@gmail.com>
2021-12-01 11:04:39 -0600

    put resume in about page

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -3,7 +3,7 @@ function Home(props) {
     <div>
         <img className="home"src="https://imgur.com/eu0mBWw.png"/>
     <h1>Welcome!</h1>
-    <p> I am a junior software Engineer located in Joliet Illinois. Go to the about or projects pages to learn more!</p>
+    <p> I am a junior software Engineer located in Joliet Illinois. Go to the about or projects pages to learn more!<a href="https://mail-attachment.googleusercontent.com/attachment/u/0/?ui=2&ik=ddfb4a75ae&attid=0.1&permmsgid=msg-f:1717963877011897250&th=17d76f0b423f3ba2&view=att&disp=inline&saddbat=ANGjdJ9BxkH512_X_RitFooYbECOCUQigFpnQ0Bd8kfwf_isb9y0WKTkpZWuvYxZ3CRXaFP73NJHGGXVLkn689RvVGMzlaL3tQ2jtngjswr-O3QPC9ggML-yewGVgDKF_nPr_DJLBbTc05pVvo1d5Qu7lm9kOp_WCYR73SJ5V9WVUKChVIEm8qOdFBlEI8oB1cAIfMQSFQ_aKLC4vo7Y8ExKdyhXxQxthYlDsxqJNiloiBxoThkWrBbMeXlhvHQk5etryoy8xwk_ufAm75IvRqmoVWjCrbtHRiuM2P1Yx3ruKErAnClcjoo1iREawHWVi9AZpAbA0rUU8wz4AsOxwMrQ2reyfTagMb1qEfshc2afoasTF7UFiqUWOCTkw0waWGrbOS3RWj8LTnc0OKfpM2JFC3Nio_IDGkkrb5mT--XtRF-kjBeIF_6O3fc_muAmeBfNsesCuOUqV1x-jPA_K7II2M0d2y0MiiPW6XXYfuGJSw8v-9enWrxIwueUdEH-MZMoaS_hbXW-ujIAGs3EMVEgSn2HV8lZ1DXv8cRZQdF8LQVmh9-KfRMjBH9y6R5uZuIc8jmhGrVkRceUulGBnFVTc8qWpRNHdDi4H9wai6aeWzbccYkWFrnwV1wgcXy64jIbz47uzXuFgNgDzT3zHqwuuPH-GVE02xzQmRglYRNoz-4IbEoUqettXDS0w-I">Resume</a><br></br></p>
     </div>
         )
 }
